96447d62c46d6976a87268c329b885cebc91e4fb
AllanEl <allanrnelias@gmail.com>
2022-11-04 06:27:25 -0400

    added right version of inquirer

--- index.js
+++ index.js
@@ -8,7 +8,32 @@ inquirer
       type:'input',
       message: 'What is the title of your project ?',
       name: 'title',
-    }
+    },
+    {
+      type:'input',
+      message: '',
+      name: 'Description',
+    },
+    {
+      type:'input',
+      message: '',
+      name: 'installation instructions',
+    },
+    {
+        type:'input',
+        message: '',
+        name: 'usage information',
+      },
+    {
+        type:'input',
+        message: '',
+        name: 'contribution guidelines',
+      },
+    {
+        type:'input',
+        message: '',
+        name: 'test instructions',
+      },
     ])
     .then((response) => {
         console.log(response)
